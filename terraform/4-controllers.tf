a032a8841d6f160748c8bc2b788ec32472daf116
Jun <steve.jh.kang@gmail.com>
2019-11-05 21:44:17 +0900

    Update 4-controllers.tf

--- terraform/4-controllers.tf
+++ terraform/4-controllers.tf
@@ -61,37 +61,6 @@ resource "aws_instance" "controller_etcd" {
 ## Kubernetes API Load Balancer
 ###############################
 
-resource "aws_elb" "kubernetes_api" {
-    name = "${var.elb_name}"
-    instances = ["${aws_instance.controller.*.id}"]
-    subnets = ["${aws_subnet.kubernetes.id}"]
-    cross_zone_load_balancing = false
-
-    security_groups = ["${aws_security_group.kubernetes_api.id}"]
-
-    listener {
-      lb_port = 6443
-      instance_port = 6443
-      lb_protocol = "TCP"
-      instance_protocol = "TCP"
-    }
-
-    health_check {
-      healthy_threshold = 2
-      unhealthy_threshold = 2
-      timeout = 15
-      target = "HTTPS:6443/"
-      interval = 30
-    }
-
-    tags = "${merge(
-      local.common_tags,
-        map(
-          "Name", "kubernetes",
-          "Owner", "${var.owner}"
-        )
-    )}"
-}
 
 ############
 ## Security
@@ -130,6 +99,4 @@ resource "aws_security_group" "kubernetes_api" {
 ## Outputs
 ############
 
-output "kubernetes_api_dns_name" {
-  value = "${aws_elb.kubernetes_api.dns_name}"
-}
+
